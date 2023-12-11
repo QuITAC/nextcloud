85ec245481b11e4b42d444eb69592d46d1790875
greedykangaroo01 <mail@christian-koch.eu>
2023-12-11 20:39:53 +0000

    Fix all important warning messages in Nextcloud admin interface and update README

--- config/additional.config.php
+++ config/additional.config.php
@@ -1,5 +1,9 @@
 <?php
 $CONFIG = array (
+  'default_phone_region' => 'DE',
   'overwriteprotocol' => 'https',
+  'trusted_proxies' => array(
+    0 => '192.168.176.2',
+  ),
   'oidc_login_auto_redirect' => true
 );
