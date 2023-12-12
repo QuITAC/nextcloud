96a47984613bccefd26b9bb43625bfc16d9d7b75
greedykangaroo01 <mail@christian-koch.eu>
2023-12-12 09:50:12 +0000

    remove one-time cron job command

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 docker compose up -d
-docker exec -d -u www-data nextcloud-app php /var/www/html/cron.php
 docker exec -u www-data nextcloud-app php /var/www/html/occ config:app:set --value=0 user_oidc allow_multiple_user_backends
