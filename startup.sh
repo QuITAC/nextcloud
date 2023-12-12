214e64e4dcbe85d0d9be0ccc5cfd645db3ae3b11
greedykangaroo01 <mail@christian-koch.eu>
2023-12-12 15:06:59 +0000

    add default dashboard config for new users

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 docker compose up -d
-docker exec -u www-data nextcloud-app php /var/www/html/occ config:app:set --value=0 user_oidc allow_multiple_user_backends
+docker exec -u www-data nextcloud-app php /var/www/html/occ config:app:set dashboard layout --value="welcome,calendar,spreed,recommendations"
+docker exec -u www-data nextcloud-app php /var/www/html/occ config:app:set user_oidc allow_multiple_user_backends --value=0
