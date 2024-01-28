79e870423df1e9853c2d85534ac1947118f916fc
JIrgend <32596131+JIrgend@users.noreply.github.com>
2024-01-28 16:36:31 +0000

    Update additional.config.php
    added default locale (de_DE) for nc additional.config.php

--- config/additional.config.php
+++ config/additional.config.php
@@ -1,9 +1,10 @@
 <?php
 $CONFIG = array (
   'default_phone_region' => 'DE',
+  'default_locale' => 'de_DE', //defualt locale german
   'overwriteprotocol' => 'https',
   'trusted_proxies' => array(
-    0 => '192.168.176.2',
+    0 => '192.168.176.2', // wtf ist das?
   ),
-  'oidc_login_auto_redirect' => true
+  'oidc_login_auto_redirect' => true  
 );
